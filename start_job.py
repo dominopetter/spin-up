d93af79bcef4d9898f912212fd8b8cddbcf7117f
Petter Olsson <petter.olsson@dominodatalab.com>
2023-10-08 06:58:14 -0700

    updated start file
    Pushed from Domino: https://nvs.domino-eval.com/workspace/petter/spin-up?executionId=6522b2ac0dc945644f0031a3

--- start_job.py
+++ start_job.py
@@ -20,12 +20,21 @@ def start_job():
 
     # Set the payload from the JSON file
     data = {
-        "projectId": "651a0d1c4c3fc9080dec1b99",
+        "projectId": "6522b265eee6474600a1b418",
         "commandToRun": "main.py",
         "overrideHardwareTierId": "large-k8s",
         "environmentId": "6519f7d2c507a51d7d5c3876",
-        "title": "API Started"
+        "title": "PYTHON API Started"
     }
 
+    # Set the payload from the JSON file
+    dataSAS = {
+        "projectId": "6522b265eee6474600a1b418",
+        "commandToRun": "main.sas",
+        "overrideHardwareTierId": "large-k8s",
+        "environmentId": "651b17ba8f83f33663a335ec",
+        "title": "SAS API Started"
+    }
     # Make the POST request
     response = requests.post(url, headers=headers, data=json.dumps(data))
+    response = requests.post(url, headers=headers, dataSAS=json.dumps(data))
